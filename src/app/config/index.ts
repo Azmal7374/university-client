ef2e69ace5fc0275650393c2c6e47138c7858edf
Azmal <azmalgazi8@gmail.com>
2024-10-23 23:45:28 +0600

    mission one completed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,16 +4,21 @@ import path from 'path';
 dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV : process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-  jwt_acess_token_secret: process.env.JWT_ACCESS_SECRET,
-  access_token_expires_in: process.env.ACCESS_TOKEN_EXPIRES_IN,
-  jwt_refresh_token_secret: process.env.JWT_REFRESH_TOKEN_SECRET,
-  refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN,
-  store_id: process.env.STORE_ID,
-  signature_key: process.env.SIGNATURE_KEY,
-  payment_url: process.env.PAYMENT_URL,
-  payment_verification_url: process.env.PAYMENT_VERIFICATION_URL,
+  bcrypt_salt_rounds:process.env.BCRYPT_SALT_ROUNDS,
+  default_password:process.env.DEFAULT_PASS,
+  jwt_access_secret:process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret:process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in:process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in:process.env.
+  JWT_ACCESS_EXPIRES_IN,
+  reset_pass_ui_link:process.env.RESET_PASS_UI_LINK, 
+  cloudinary_cloud_name:process.env.CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret:process.env.CLOUDINARY_SECRET_KEY,
+
 };
+
+
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface TUser {
+    id: string;
+    email:string;
+    password: string;
+    needsPasswordChange: boolean;
+    passwordChangedAt?: Date;
+    role: 'admin' | 'student' | 'faculty';
+    status: 'in-progress' | 'blocked';
+    isDeleted: boolean;
+  }
+  
